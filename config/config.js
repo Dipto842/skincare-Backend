59edc66461fcf642ad1e2829ef6b24ab4abe2a23
Dipto842 <diptobakshi72@gmail.com>
2025-09-13 18:20:37 +0600

    add all

--- config/config.js
+++ config/config.js
@@ -4,7 +4,11 @@ import mongoose from 'mongoose';
 const connectDB= async()=>{
     try{
         await mongoose.connect('mongodb+srv://skincare:TfAdRMlDrEiEW3JU@cluster0.ssb3nmc.mongodb.net/skincare?retryWrites=true&w=majority&appName=Cluster0')
-        console.log("MongoDB connected");
+        console.log("MongoDB connected",{
+             useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000 
+        });
         
     }
     catch(err){
